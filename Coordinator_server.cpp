6b029eb4160842f4a3572af5698602d09ac4c20c
pheotter <romotty@gmail.com>
2025-02-25 14:38:32 -0600

    fix segmentation fault

--- Coordinator_server.cpp
+++ Coordinator_server.cpp
@@ -74,9 +74,9 @@ class CoordinatorHandler : virtual public CoordinatorIf {
     std::vector<std::vector<double>> W;
     std::vector<std::vector<double>> V;
 
-    almighty.get_weights(shared_weights.V, shared_weights.W);
-    std::cout << "v's size" << shared_weights.V.size() << endl;
-    std::cout << "w's size" << shared_weights.W.size() << endl;
+    almighty.get_weights(V, W);
+    std::cout << "v's size" << V.size() << endl;
+    std::cout << "w's size" << W.size() << endl;
 
     // // Convert vector<vector<double>> to list<list<double>>
     // for (const auto& row:W) {
